29f78eff186c7c9e752257bd3c41f14c2b5fa416
Rylan Mcneil <rylanmcneil@hotmail.com>
2022-10-31 13:10:33 +1100

    Text Blue

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="">NFT DAPP</h1>
+      <h1 className="text-blue-700">NFT DAPP</h1>
     </div>
   );
 };
